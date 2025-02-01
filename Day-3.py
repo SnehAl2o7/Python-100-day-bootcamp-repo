68402ae34648a782b71fc92fd97bd49bfee4d285
Snehal <snehalsingh353@gmail.com>
2025-02-01 14:37:38 +0530

    initial commit

--- Day-3.py
+++ Day-3.py
@@ -45,6 +45,28 @@ else :
     print("You are overweight")
 
 #PRACTICE NO-5
+print("Welcome to the Love calculator")
+name1 = input("What is your name: \n")
+name2 = input("What is their name: \n")
+combined_str = name1+name2
+lower = combined_str.lower()
+t = lower.count('t')
+r = lower.count('r')
+u = lower.count('u')
+e = lower.count('e')
+true = t+r+u+e
+l = lower.count('l')
+o = lower.count('o')
+v = lower.count('v')
+e = lower.count('e')
+love = l+o+v+e
+love_score = int(str(true) + str(love))
+if(love_score < 0) or (love_score > 90):
+    print(f"Your love score is {love_score}, you go together like coke and mentos")
+elif(love_score >= 40) and (love_score <= 50):
+    print(f"Your love score is {love_score}, you are alright together")
+else:
+    print(f"Your love score is {love_score}") 
 
 
 #PROJECT NUMBER- 3
